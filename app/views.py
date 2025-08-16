888934f68bf508334496b97748fa493c30973a99
Ardianto <108719126+ardiantoosman4@users.noreply.github.com>
2025-08-16 08:31:30 +0700

    improve ui login register, and logic button navbar

--- app/views.py
+++ app/views.py
@@ -1,11 +1,21 @@
-from flask import Blueprint, render_template
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask import Blueprint, render_template, redirect, url_for
+from flask_jwt_extended import jwt_required, get_jwt_identity, verify_jwt_in_request
 
 views_bp = Blueprint("views", __name__)
 
 @views_bp.get("/")
 def home():
-    return render_template("login.html")
+    try:
+        # Check if JWT exists and is valid
+        verify_jwt_in_request(optional=True)
+        user = get_jwt_identity()
+        if user:
+            # User logged in, redirect to dashboard
+            return redirect(url_for("views.dashboard"))
+    except Exception:
+        user = None
+    # User not logged in
+    return redirect(url_for("auth.login_get"))
 
 @views_bp.get("/dashboard")
 @jwt_required()
